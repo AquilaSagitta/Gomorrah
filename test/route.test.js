682637feaf136b9e67182887aaa94844e486680c
Chris Blahut <blahut.chris@gmail.com>
2019-10-07 13:48:21 -0500

    Completed spits out names of systems with NPC kills in range of Osaa

--- test/route.test.js
+++ test/route.test.js
@@ -18,7 +18,7 @@ describe('GET /', () => {
 
     it('responds with 200', async () => {
         const res = await server.inject({
-            method: 'get',
+            method: 'GET',
             url: '/'
         });
         expect(res.statusCode).to.equal(200);
@@ -38,10 +38,57 @@ describe('Unknown route', () => {
 
     it('redirects to /', async () => {
         const res = await server.inject({
-            method: 'get',
+            method: 'GET',
             url: '/unknown/route'
         });
         expect(res.statusCode).to.equal(302);
         expect(res.headers.location).to.equal('/');
+    });
+});
+
+describe('GET /system/{systemName}', () => {
+    let server;
+
+    beforeEach(async () => {
+        server = await init();
+    });
+
+    afterEach(async () => {
+        await server.stop();
+    });
+
+    it('responds with 200', async () => {
+        const res = await server.inject({
+            method: 'GET',
+            url: '/system/30004191'
+        });
+        expect(res.statusCode).to.equal(200);
+        const nameObj = JSON.parse(res.result);
+        expect(nameObj.name).to.equal('C2-1B5');
+    });
+});
+
+describe('POST /names', () => {
+    let server;
+
+    beforeEach(async () => {
+        server = await init();
+    });
+
+    afterEach(async () => {
+        await server.stop();
+    });
+
+    it('responds with 200', async () => {
+        const res = await server.inject({
+            method: 'POST',
+            url: '/names',
+            payload: [
+                '30004191'
+            ]
+        });
+        expect(res.statusCode).to.equal(200);
+        const nameObj = JSON.parse(res.result);
+        expect(nameObj[0].name).to.equal('C2-1B5');
     });
 });
